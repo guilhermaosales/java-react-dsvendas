cf6b91f8c6c31b44d7a9eb7dff6b3ef950be36ea
Guilherme Sales <guilhermewrsales@gmail.com>
2021-09-08 16:30:51 -0300

    Static charts

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,9 +2,9 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App desenvolvido por <a href="https://github.com/guilhermao" target="_blank" rel="noreferrer">Guilherme Sales</a></p>
+                <p className="text-light">App desenvolvido por <a href="https://github.com/guilhermao" className="text-decoration-none" target="_blank" rel="noreferrer">Guilherme Sales</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                    Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" className="text-decoration-none" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
         </footer>
     );
