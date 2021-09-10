ab4bcef72de059f7a4c66f4f2c59f5a68db5b4ee
Guilherme Sales <guilhermewrsales@gmail.com>
2021-09-10 14:41:50 -0300

    Pagination

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -24,6 +24,7 @@ const Dashboard = () => {
                 <div className="py-3">
                     <h2 className="text-primary">All sales</h2>
                 </div>
+                
                 <DataTable />
             </div>
             <Footer />
