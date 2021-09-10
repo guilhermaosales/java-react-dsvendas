23f6909c4af5abc5eab5d64daf88b195305d2463
Guilherme Sales <guilhermewrsales@gmail.com>
2021-09-10 10:38:41 -0300

    Home, navigation

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -2,9 +2,9 @@ const Footer = () => {
     return (
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
-                <p className="text-light">App developed by <a href="https://github.com/guilhermao" className="text-decoration-none" target="_blank" rel="noreferrer">Guilherme Sales</a></p>
+                <p className="text-light">App developed by <a href="https://github.com/guilhermao" target="_blank" rel="noreferrer">Guilherme Sales</a></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
-                    Event promoted by DevSuperior: <a href="https://instagram.com/devsuperior.ig" className="text-decoration-none" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+                    Event promoted by DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
         </footer>
     );
